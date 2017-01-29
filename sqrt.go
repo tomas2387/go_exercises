ed16e5ddbac6bba4f5cc7ad313a2f5d39911c1d4
Tomás <tomas2387@gmail.com>
2017-01-29 11:52:30 +0100

    #20 Exercise: Errors

--- sqrt.go
+++ sqrt.go
@@ -5,17 +5,31 @@ import (
 	"math"
 )
 
-func SqrtFast(x float64) float64 {
+type ErrNegativeSqrt float64
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprint("cannot Sqrt negative number: ", float64(e))
+}
+
+
+func SqrtFast(x float64)  (float64, error) {
+	if x < 0 {
+		return 0, ErrNegativeSqrt(x)
+	}
+
 	z := float64(1)
 	for i:=1; i < 10; i++ {
 		dividend := math.Pow(z, 2) - x
 		divisor := float64(2) * z
 		z = z - (dividend / divisor)
 	}
-	return z
+	return z, nil
 }
 
-func SqrtRight(x float64) float64 {
+func SqrtRight(x float64)  (float64, error) {
+	if x < 0 {
+		return 0, ErrNegativeSqrt(x)
+	}
+
 	z := float64(1)
 	delta := float64(1)
 	for  delta > 1e-10 {
@@ -25,9 +39,22 @@ func SqrtRight(x float64) float64 {
 		z = z - (dividend / divisor)
 		delta = math.Abs(z - lastZ)
 	}
-	return z
+	return z, nil
 }
 
 func main() {
-	fmt.Printf("This is fast %v \nAnd this is correct %v \nBut all must equal %v", SqrtFast(2), SqrtRight(2), math.Sqrt(2))
+	fast, err := SqrtFast(2)
+	if err != nil {
+		fmt.Println(err);
+		return
+	}
+	correct, err := SqrtRight(2)
+	if err != nil {
+		fmt.Println(err);
+		return
+	}
+
+	original := math.Sqrt(2)
+
+	fmt.Printf("This is fast %v \nAnd this is correct %v \nBut all must equal %v", fast, correct, original)
 }
